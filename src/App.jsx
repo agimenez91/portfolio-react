119882bcaec3e69219dc5735d82458cbf1b97986
agimenez91 <andrea.gimenez91@gmail.com>
2023-05-28 11:22:57 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes } from "react-router-dom"
 import { Route } from "react-router-dom"
 import "./globalStyles/GlobalStyle.scss"
-import Topbar from "./components/TopBar/Topbar"
 import Home from "./views/home"
 import Contact from "./views/contact"
 import Project from "./views/Project"
@@ -9,6 +8,7 @@ import Cookies from "./views/Cookies"
 import Legal from "./views/Legal"
 import Privacy from "./views/Privacy"
 import Error404 from "./views/Error404"
+import Topbar from "./components/TopBar/Topbar"
 
 
 function App() {
