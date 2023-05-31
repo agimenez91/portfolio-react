b5b6bf9847c8b479d109fab03ccd39051a8c2b6a
agimenez91 <andrea.gimenez91@gmail.com>
2023-05-31 20:47:41 +0200

    update may 31st 2

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { projectContext } from "../App";
 import Cover from "../components/Cover/Cover"
 import Filter from "../components/Filter/Filter"
 import ProjectCard from "../components/ProjectCard/ProjectCard"
 
 function Home() {
+  const {projects} = useContext (projectContext);
+
 
     return (
       <>
