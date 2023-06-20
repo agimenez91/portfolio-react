b52e179a2af5f54429f86e5a8153582585a6b1f5
agimenez91 <andrea.gimenez91@gmail.com>
2023-06-20 18:34:15 +0200

    change on message cover

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -8,8 +8,7 @@ function Cover() {
       <>
         <div className="cover">
           <TypographicEffect></TypographicEffect>
-          <p className="big">I am a Junior developer who aims to create compositions that are aesthetically pleasing and easy to understand for all audiences.
-          Feel free to explore my portfolio below (Sort projects by type or click on them to access more detailed information).
+          <p className="big">A frontend developer who aims to create compositions that are aesthetically pleasing and easy to understand for all audiences.
           <br/>I hope you enjoy exploring my work!</p>
           <p className="small">P.S. If you would like to know more about me personally, don't forget to visit my <a href="/project/about">about</a>!</p>
         </div>
