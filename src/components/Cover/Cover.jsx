c2fd37fe4aa6456e9512813ede44729b6e787946
agimenez91 <andrea.gimenez91@gmail.com>
2023-06-21 22:51:56 +0200

    project card hover fixed

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -8,9 +8,7 @@ function Cover() {
       <>
         <div className="cover">
           <TypographicEffect></TypographicEffect>
-          <p className="big">Frontend developer who aims to create compositions that are aesthetically pleasing and easy to understand for all audiences.
-          <br/>I hope you enjoy exploring my work!</p>
-          <p className="small">P.S. If you would like to know more about me personally, don't forget to visit my <a href="/project/about">about</a>!</p>
+          <p className="big">Frontend developer who aims to create compositions that are aesthetically pleasing and easy to understand for all audiences.</p>
         </div>
       </>
     )
